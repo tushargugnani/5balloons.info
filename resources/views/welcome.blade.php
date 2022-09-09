152e429f45d382b602860d425211eebee41e69fb
tapan288 <tapansharma69@gmail.com>
2022-09-09 11:13:45 +0545

    fix page title issue

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.app')
 
+@section('title', '5 Balloons - Practicle Programming and Tech Articles')
+
 @section('content')
     <div class="container max-w-screen-lg mx-auto px-5 md:px-10 mt-10">
         @include('partials.hero')
@@ -7,7 +9,5 @@
         @include('partials.icons')
 
         @include('partials.posts')
-
-        @include('partials.pagination')
     </div>
 @endsection
