70300b92813eb7665b04f93ca7d02d479621e7a2
tapan288 <tapansharma69@gmail.com>
2022-09-13 16:51:13 +0545

    fix: icon links on home page

--- resources/views/partials/icons.blade.php
+++ resources/views/partials/icons.blade.php
@@ -4,27 +4,27 @@
         <span class="text-yellow-800 font-semibold">Stack</span>
     </h2>
     <div class="flex justify-center mt-5 flex-wrap">
-        <a href="#">
+        <a href="{{ config('app.url') }}/laravel-tutorials-examples">
             <div class="rounded border p-3 m-2 cursor-pointer bg-white hover:bg-yellow-100 hover:scale-105">
                 <img width="100px" src="{{ asset('assets/icons/laravel.png') }}" alt="Laravel Roadmap">
             </div>
         </a>
-        <a href="#">
+        <a href="{{ config('app.url') }}/category/laravel-livewire">
             <div class="rounded border p-3 m-2 cursor-pointer bg-white hover:bg-yellow-100 hover:scale-105">
                 <img width="100px" src="{{ asset('assets/icons/livewire.png') }}" alt="Livewire Roadmap">
             </div>
         </a>
-        <a href="#">
+        <a href="{{ config('app.url') }}/category/tailwindcss">
             <div class="rounded border p-3 m-2 cursor-pointer bg-white hover:bg-yellow-100 hover:scale-105">
                 <img width="100px" src="{{ asset('assets/icons/tailwindcss.png') }}" alt="Tailwind CSS Roadmap">
             </div>
         </a>
-        <a href="#">
+        <a href="{{ config('app.url') }}/category/bootstrap">
             <div class="rounded border p-3 m-2 cursor-pointer bg-white hover:bg-yellow-100 hover:scale-105">
                 <img width="100px" src="{{ asset('assets/icons/bootstrap.png') }}" alt="Bootstrap Roadmap">
             </div>
         </a>
-        <a href="#">
+        <a href="{{ config('app.url') }}/category/alpinejs">
             <div class="rounded border p-3 m-2 cursor-pointer bg-white hover:bg-yellow-100 hover:scale-105">
                 <img width="100px" src="{{ asset('assets/icons/alpinejs.png') }}" alt="Alpine.js Roadmap">
             </div>
