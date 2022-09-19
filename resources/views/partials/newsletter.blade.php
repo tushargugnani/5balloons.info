03af849deafab157a201031ae93d42381791ab19
Tushar <tushar.gugnani@gmail.com>
2022-09-19 14:56:18 +0530

    newsletter UI

---
+++ resources/views/partials/newsletter.blade.php
@@ -0,0 +1,20 @@
+<div>
+  <div class="mx-auto lg:max-w-4xl xl:max-w-6xl px-4 py-2 sm:px-6 lg:py-16 lg:px-8">
+    <div class="rounded-lg bg-orange-500 px-6 py-6 md:py-12 md:px-12 lg:py-16 lg:px-16 xl:flex xl:items-center">
+      <div class="xl:w-0 xl:flex-1">
+        <h2 class="text-2xl font-bold tracking-tight text-white sm:text-3xl">Email Newsletter</h2>
+        <p class="mt-3 max-w-3xl text-lg leading-6 text-white">Sign up for our email newsletter to stay up to date.</p>
+      </div>
+      <div class="mt-8 sm:w-full sm:max-w-md xl:mt-0 xl:ml-8">
+        <form class="sm:flex">
+          <label for="email-address" class="sr-only">Email address</label>
+          <input id="email-address" name="email-address" type="email" autocomplete="email" required class="w-full rounded-md border-white px-5 py-3 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-yellow-700" placeholder="Enter your email">
+          <button type="submit" class="mt-3 flex w-full items-center justify-center rounded-md border border-transparent bg-yellow-500 px-5 py-3 text-base font-medium text-white shadow hover:bg-yellow-400 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-yellow-700 sm:mt-0 sm:ml-3 sm:w-auto sm:flex-shrink-0">Subscribe</button>
+        </form>
+        <p class="mt-3 text-sm text-amber-100">
+            We will never send any spam emails. Promise.
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
