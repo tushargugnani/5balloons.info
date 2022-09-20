06a7d6c81327881ee0288462e43056b6c7bc5bc0
Tushar <tushar.gugnani@gmail.com>
2022-09-20 11:07:25 +0530

    newsletter integration

--- resources/views/partials/newsletter.blade.php
+++ resources/views/partials/newsletter.blade.php
@@ -6,10 +6,10 @@
         <p class="mt-3 max-w-3xl text-lg leading-6 text-white">Sign up for our email newsletter to stay up to date.</p>
       </div>
       <div class="mt-8 sm:w-full sm:max-w-md xl:mt-0 xl:ml-8">
-        <form class="sm:flex">
+        <form class="sm:flex" action="https://www.getrevue.co/profile/5balloons/add_subscriber" method="post" id="revue-form" name="revue-form"  target="_blank">
           <label for="email-address" class="sr-only">Email address</label>
-          <input id="email-address" name="email-address" type="email" autocomplete="email" required class="w-full rounded-md border-white px-5 py-3 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-yellow-700" placeholder="Enter your email">
-          <button type="submit" class="mt-3 flex w-full items-center justify-center rounded-md border border-transparent bg-yellow-500 px-5 py-3 text-base font-medium text-white shadow hover:bg-yellow-400 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-yellow-700 sm:mt-0 sm:ml-3 sm:w-auto sm:flex-shrink-0">Subscribe</button>
+          <input id="member_email" name="member[email]" type="email" autocomplete="email" required class="w-full rounded-md border-white px-5 py-3 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-yellow-700" placeholder="Enter your email">
+          <button type="submit" name="member[subscribe]" id="member_submit" class="mt-3 flex w-full items-center justify-center rounded-md border border-transparent bg-yellow-500 px-5 py-3 text-base font-medium text-white shadow hover:bg-yellow-400 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-yellow-700 sm:mt-0 sm:ml-3 sm:w-auto sm:flex-shrink-0">Subscribe</button>
         </form>
         <p class="mt-3 text-sm text-amber-100">
             We will never send any spam emails. Promise.
