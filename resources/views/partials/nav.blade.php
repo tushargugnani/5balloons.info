142638d1dbc118bad07c249cc5cd2b4a4aa9bb91
Tushar <tushar.gugnani@gmail.com>
2022-09-21 11:56:42 +0530

    fix nav

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -106,13 +106,13 @@
                         </svg>
                     </a>
                     <ul x-show="open" class="right-0 top-12 z-10 bg-white absolute rounded border w-48" x-cloak>
-                        <li @click="location.href = 'https://www.udemy.com/course/laravel-dusk-beginner-to-advanced/?referralCode=31027C813DE63523361F'" class="py-2 pl-4 pr-5 hover:bg-yellow-100">
+                        <li @click="window.open('https://www.udemy.com/course/laravel-dusk-beginner-to-advanced/?referralCode=31027C813DE63523361F','_blank')" class="py-2 pl-4 pr-5 hover:bg-yellow-100">
                             <a href="https://www.udemy.com/course/laravel-dusk-beginner-to-advanced/?referralCode=31027C813DE63523361F"
                                 target="_blank">
                                 Laravel Dusk
                             </a>
                         </li>
-                        <li @click="location.href = 'https://www.youtube.com/watch?v=LGetygzyRFA&list=PLSWOV7oy-nwVpqRG6drYXNcn02A2QYh7V&ab_channel=5Balloons'" class="py-2 pl-4 pr-5 hover:bg-yellow-100">
+                        <li @click="window.open('https://www.youtube.com/watch?v=LGetygzyRFA&list=PLSWOV7oy-nwVpqRG6drYXNcn02A2QYh7V&ab_channel=5Balloons','_blank')" class="py-2 pl-4 pr-5 hover:bg-yellow-100">
                             <a href="https://www.youtube.com/watch?v=LGetygzyRFA&list=PLSWOV7oy-nwVpqRG6drYXNcn02A2QYh7V&ab_channel=5Balloons"
                                 target="_blank">
                                 AlpineJS
@@ -132,7 +132,12 @@
                     <ul x-show="open" class="right-0 top-12 z-10 bg-white absolute rounded border w-48" x-cloak>
                         <li @click="location.href = '{{ config('app.url') }}/about'" class="py-2 pl-4 pr-5 hover:bg-yellow-100">
                             <a class="w-full" href="{{ config('app.url') }}/about">
-                                About Us
+                                About Me
+                            </a>
+                        </li>
+                        <li @click="window.open('https://github.com/sponsors/tushargugnani','_blank')" class="py-2 pl-4 pr-5 hover:bg-yellow-100">
+                            <a class="w-full" href="{{ config('app.url') }}/about">
+                                Advertise
                             </a>
                         </li>
                     </ul>
