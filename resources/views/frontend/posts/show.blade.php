57958e7cb91156e13dea841db4a6f453b782db45
Tushar <tushar.gugnani@gmail.com>
2022-09-23 10:02:26 +0530

    adsense ad

--- resources/views/frontend/posts/show.blade.php
+++ resources/views/frontend/posts/show.blade.php
@@ -65,5 +65,20 @@
 
     @include('partials.left-banner-bar')
 
+    <div>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8077071189647178"
+     crossorigin="anonymous"></script>
+<!-- Horizontal Ad -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-8077071189647178"
+     data-ad-slot="6284350204"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+    </div>
+
 
 @endsection
