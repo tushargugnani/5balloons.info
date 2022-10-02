b67a774009cac607f923fa1ffe9dc64d2e4894b8
Tushar Gugnani <tushar.gugnani@gmail.com>
2022-10-02 15:40:29 +0530

    replace invester ad with adsense

--- resources/views/partials/left-banner-bar.blade.php
+++ resources/views/partials/left-banner-bar.blade.php
@@ -1,3 +1,11 @@
 <div class="fixed hidden lg:block top-24 inset-0 left-2 max-w-max z-10">
-<a target="_blank" href="https://inspector.dev/?ref=5balloons"><img src="{{ asset('assets/sponsors/inspector.gif') }}" alt="Inspector Dev"></a>
-</div>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8077071189647178"
+     crossorigin="anonymous"></script>
+<!-- Vertical Fixed Ad Unit 120 * 600 -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:120px;height:600px"
+     data-ad-client="ca-pub-8077071189647178"
+     data-ad-slot="2954531165"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script></div>
