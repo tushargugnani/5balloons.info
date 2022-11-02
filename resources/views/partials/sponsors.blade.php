fca89c4ef2a8da1eb6012042bd707a1490241f70
Tushar Gugnani <tushar.gugnani@gmail.com>
2022-11-02 16:03:39 +0530

    sponsors update

--- resources/views/partials/sponsors.blade.php
+++ resources/views/partials/sponsors.blade.php
@@ -2,32 +2,55 @@
   <h3 class="text-center mb-2 font-semibold text-amber-700">Sponsors</h3>
   <h4 class="text-center text-stone-500 mb-4">This website has <span class="font-semibold">NO spammy ads</span>, <span class="font-semibold">distracting popups</span>, and <span class="font-semibold">tracking codes</span>. Thanks to our sponsors!</h4>
   <div class="grid grid-cols-2 gap-6 text-center lg:grid-cols-5">
-    <a class="bg-amber-100  hover:bg-amber-200 border border-amber-400 rounded" href="https://www.digitalocean.com/?refcode=ee55b40d7cf6&utm_campaign=Referral_Invite&utm_medium=Referral_Program&utm_source=badge" onclick="window.fathom.trackGoal('CKCAGCDB', 0);" target="_blank" class="block">
-        <div class="flex items-center justify-center p-5 h-24">
+    <a class="border border-gray-300 rounded relative" href="https://www.digitalocean.com/?refcode=ee55b40d7cf6&utm_campaign=Referral_Invite&utm_medium=Referral_Program&utm_source=badge" onclick="window.fathom.trackGoal('CKCAGCDB', 0);" target="_blank" class="block">
+        <div class="flex items-center justify-center h-28 bg-blue-100 p-2">
             <img src="{{ asset('assets/sponsors/digital-ocean-logo.png') }}" alt="Digital Ocean Logo" class="block object-contain h-26" />
         </div>
-        <div class="text-orange-800 mb-3 text-sm px-2">($200 Credits on signing up for DigitalOcean cloud hosting platform)</div>
+        <div class="text-gray-700 mb-3 text-xs px-2 text-left pt-4"><span class="font-semibold">Digital Ocean</span> - ($200 Credits on signing up for DigitalOcean cloud hosting platform)</div>
+        <div class="h-8 flex items-center justify-center w-full text-[9px] uppercase absolute bottom-0 bg-gray-100">AFFILIATE AD</div>
     </a>
-    <a class="bg-amber-100 hover:bg-amber-200 border border-amber-400 rounded" href="https://usefathom.com/ref/HPQCFW" onclick="window.fathom.trackGoal('0LFQ9YNE', 0);" target="_blank" class="block">
-        <div class="flex items-center justify-center p-5 h-24">
-             <img src="{{ asset('assets/sponsors/fathom-logo.png') }}" alt="Fathom Analytics Logo" class="block object-contain h-26" />
+    <a class="border border-gray-300 rounded relative" href="https://usefathom.com/ref/HPQCFW" onclick="window.fathom.trackGoal('0LFQ9YNE', 0);" target="_blank" class="block">
+        <div class="flex items-center justify-center h-28 bg-gray-300 p-2">
+            <img src="{{ asset('assets/sponsors/fathom-logo.png') }}" alt="Fathom Analytics Logo" class="block object-contain h-26" />
         </div>
-        <div class="text-orange-800 mb-3 text-sm px-2">($10 Credits on signing up for Fathom Analytics web analytics tool)</div>
+        <div class="text-gray-700 mb-3 text-xs px-2 text-left pt-4"><span class="font-semibold">Fathom Analytics</span> - ($10 Credits on signing up for Fathom Analytics web analytics tool)</div>
+        <div class="h-8 flex items-center justify-center w-full text-[9px] uppercase absolute bottom-0 bg-gray-100">AFFILIATE AD</div>
     </a>
-    <a href="https://a.paddle.com/v2/click/103161/142447?link=2106" target="_blank" class="block bg-amber-100 hover:bg-amber-200 border border-amber-400 rounded">
-        <div class="flex items-center justify-center p-5 h-24">
-            <img src="{{ asset('assets/sponsors/tinkerwell_logo.png') }}" alt="Tinkerwell Logo" class="block object-contain w-14 h-14" />
+    <a class="border border-gray-300 rounded relative" href="https://a.paddle.com/v2/click/103161/142447?link=2106" target="_blank" class="block">
+        <div class="flex items-center justify-center h-28 bg-purple-300 p-2">
+            <img src="{{ asset('assets/sponsors/tinkerwell_logo.png') }}" alt="Fathom Analytics Logo" class="block object-contain h-12" />
         </div>
-        <div class="text-orange-800 mb-3 text-sm px-2">(Tinkerwell - The code runner for PHP, works with Laravel, Wordpress etc.)</div>
+        <div class="text-gray-700 mb-3 text-xs px-2 text-left pt-4"><span class="font-semibold">Tinkerwell</span> - (The code runner for PHP, works with Laravel, Wordpress etc.)</div>
+        <div class="h-8 flex items-center justify-center w-full text-[9px] uppercase absolute bottom-0 bg-gray-100">AFFILIATE AD</div>
     </a>
+    <div>
+        <div id="carbon__js" class="native-carbon"></div>
+        <script>
+        (function(){  
+        if(typeof _bsa !== 'undefined' && _bsa) {
+            _bsa.init('custom', 'CEAI5KQM', 'placement:5balloonsinfo-custom',
+                {
+                target: '#carbon__js',
+                template: `
+        <div class="native-container">
+        <a href="##link##" class="native-banner" rel="sponsored noopener" target="_blank" title="##company## — ##tagline##">
+                <div class="native-img" style="background: ##backgroundColor##;"><img src="##logo##"></div>
+                <div class="native-details">
+                <span class="native-desc"><strong>##company##</strong> — ##description##</span>
+                </div>
+            </a>
+            <a class="native-via" href="##adViaLink##"">Ad via BuySellAds</a>
+        </div>
+        `,
+                }
+            );
+            }
+        })();
+        </script>    
+    </div>
     <a href="https://github.com/sponsors/tushargugnani" target="_blank" class="block bg-amber-100 hover:bg-amber-200 border border-amber-400 rounded">
         <div class="flex items-center justify-center p-5 h-48">
-        <div class="text-gray-500">Your Logo Here</div>
-        </div>
-    </a>
-    <a href="https://github.com/sponsors/tushargugnani" target="_blank" class="block bg-amber-100 hover:bg-amber-200 border border-amber-400 rounded">
-        <div class="flex items-center justify-center p-5 h-48">
-        <div class="text-gray-500">Your Logo Here</div>
+        <div class="text-gray-500">Your Ad Here</div>
         </div>
     </a>
   </div>
