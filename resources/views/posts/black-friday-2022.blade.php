442e2d42f76edcac3fc6f40787dc023198203676
Tushar Gugnani <tushar.gugnani@gmail.com>
2022-11-15 11:58:01 +0530

    deals

--- resources/views/posts/black-friday-2022.blade.php
+++ resources/views/posts/black-friday-2022.blade.php
@@ -508,6 +508,48 @@
                             </table>
                         </div>
                     </div>
+
+                    <!-- TailwindCSS Tools -->
+                    <div class="my-3">
+                        <div class="flex items-center ml-5 text-emerald-500">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-pink-800 mr-3">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M11.42 15.17L17.25 21A2.652 2.652 0 0021 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 11-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 004.486-6.336l-3.276 3.277a3.004 3.004 0 01-2.25-2.25l3.276-3.276a4.5 4.5 0 00-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437l1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008z" />
+                        </svg>
+
+                            <h4 class=" text-xl my-2">TailwindCSS Tools</h4>
+                        </div>
+                        <div class="">
+                            <table class=" divide-y divide-gray-300">
+                                <thead class="bg-gray-50">
+                                <tr>
+                                    <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-base font-semibold text-gray-900 sm:pl-6">Name</th>
+                                    <th scope="col" class="px-3 py-3.5 text-left text-base font-semibold text-gray-900">Description</th>
+                                    <th scope="col" class="px-3 py-3.5 text-left text-base font-semibold text-gray-900">Deal Details</th>
+                                    <th scope="col" class="px-3 py-3.5 text-left text-base font-semibold text-gray-900">Valid Till</th>
+                                </tr>
+                                </thead>
+                                <tbody class="divide-y divide-gray-200 bg-white">
+                                    <tr>
+                                        <td class="py-4 pl-4 pr-3 text-base font-medium text-gray-900 sm:pl-6">
+                                            <a href="https://a.paddle.com/v2/click/103161/142447?link=3094" target="_blank" rel="noopener">Windy</a> by <a href="https://beyondco.de/" target="_blank">Beyond Code</a>
+                                        </td>
+                                        <td class="px-3 py-4 text-base text-gray-500">Copy elements from any website as a Tailwind CSS component</td>
+                                        <td class="px-3 py-4 text-base text-gray-500">TBA</td>
+                                        <td class="px-3 py-4 text-base text-gray-500">TBA</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="py-4 pl-4 pr-3 text-base font-medium text-gray-900 sm:pl-6">
+                                            <a href="https://a.paddle.com/v2/click/103161/142447?link=3180" target="_blank" rel="noopener">Tint</a> by <a href="https://beyondco.de/" target="_blank">Beyond Code</a>
+                                        </td>
+                                        <td class="px-3 py-4 text-base text-gray-500">The color picker for Tailwind CSS developers.</td>
+                                        <td class="px-3 py-4 text-base text-gray-500">TBA</td>
+                                        <td class="px-3 py-4 text-base text-gray-500">TBA</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+
                     <!-- TailwindCSS Courses -->
                     <div class="my-3">
                         <div class="flex items-center ml-5 text-emerald-500">
