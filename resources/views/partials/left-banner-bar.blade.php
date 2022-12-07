9daaf90d98208952626f9ace663d18580c1c24e5
Tushar Gugnani <tushar.gugnani@gmail.com>
2022-12-07 11:48:42 +0530

    left banner

--- resources/views/partials/left-banner-bar.blade.php
+++ resources/views/partials/left-banner-bar.blade.php
@@ -1,11 +1,17 @@
 <div class="fixed hidden lg:block top-24 inset-0 left-2 max-w-max z-10">
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8077071189647178"
-     crossorigin="anonymous"></script>
-<!-- Vertical Fixed Ad Unit 120 * 600 -->
-<ins class="adsbygoogle"
-     style="display:inline-block;width:120px;height:600px"
-     data-ad-client="ca-pub-8077071189647178"
-     data-ad-slot="2954531165"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script></div>
+<div class="w-48 bg-white">
+<div class="flex justify-center">
+  <div class="rounded-lg shadow-lg bg-white max-w-sm">
+    <a href="https://gumroad.com/a/941530323/NHefI" target="_blank">
+      <img class="" src="https://wp.5balloons.info/wp-content/uploads/2022/12/tGBTIoM3NuOLl65lFHm4uark6cY2.jpg" alt="" class="object-fill"/>
+    </a>
+    <div class="p-3">
+      <h5 class="text-gray-900 text-xl font-medium mb-2">Untitled UI</h5>
+      <p class="text-gray-700 text-base mb-4">
+          Untitled UI is the largest UI kit and design system for Figma in the world. Kickstart any project, save thousands of hours, and level up as a designer.
+      </p>
+      <a href="https://gumroad.com/a/941530323/NHefI" target="_blank" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">GET this</a>
+    </div>
+  </div>
+</div>
+</div>
