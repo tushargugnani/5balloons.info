c7c1cb1cab51ede237294f0a03e8af460bd5967d
Tushar Gugnani <tushar.gugnani@gmail.com>
2023-01-11 14:41:47 +0530

    move ads to smart ad package

--- resources/views/partials/sponsors.blade.php
+++ resources/views/partials/sponsors.blade.php
@@ -2,27 +2,15 @@
   <h3 class="text-center mb-2 font-semibold text-amber-700">Sponsors</h3>
   <h4 class="text-center text-stone-500 mb-4">This website has <span class="font-semibold">NO spammy ads</span>, <span class="font-semibold">distracting popups</span>, and <span class="font-semibold">tracking codes</span>. Thanks to our sponsors!</h4>
   <div class="grid grid-cols-2 gap-6 text-center lg:grid-cols-4" id="sponsors">
-    <a class="border border-gray-300 rounded relative hover:bg-purple-100" href="https://gumroad.com/a/950437075/VXzEu" onclick="window.fathom.trackGoal('0LFQ9YNE', 0);" target="_blank" class="block">
-        <div class="flex items-center justify-center h-48 bg-white-300 shadow">
-            <img src="https://wp.5balloons.info/wp-content/uploads/2022/12/tailkit.png" alt="TailKit Cover Image" class="block object-fit w-full h-full" />
-        </div>
-        <div class="text-gray-700 mb-5 text-sm px-2 text-left pt-4 pb-6"><span class="font-semibold text-lg">Tailkit</span> - Carefully crafted, fully responsive UI Components, Templates and Tools for your Tailwind CSS based projects.</div>
-        <div class="h-8 flex items-center justify-center w-full text-[9px] uppercase absolute bottom-0 bg-gray-100 text-indigo-600">AFFILIATED</div>
-    </a>
-    <a class="border border-gray-300 rounded relative" href="https://a.paddle.com/v2/click/103161/142447?link=2106" target="_blank" class="block">
-        <div class="flex items-center justify-center h-48 bg-purple-300 p-2">
-            <img src="{{ asset('assets/sponsors/tinkerwell_logo.png') }}" alt="Fathom Analytics Logo" class="block object-contain h-12" />
-        </div>
-        <div class="text-gray-700 mb-3 text-sm px-2 text-left pt-4 pb-6"><span class="font-semibold text-lg">Tinkerwell</span> - (The code runner for PHP, works with Laravel, Wordpress etc.)</div>
-        <div class="h-8 flex items-center justify-center w-full text-[9px] uppercase absolute bottom-0 bg-gray-100 text-indigo-600">AFFILIATED</div>
-    </a>
-    <a class="border border-gray-300 rounded relative hover:bg-purple-100" href="https://calebporzio.gumroad.com/l/SFYKpD/BFRI21?a=912288051" target="_blank" class="block">
+
+
+    <!-- <a class="border border-gray-300 rounded relative hover:bg-purple-100" href="https://calebporzio.gumroad.com/l/SFYKpD/BFRI21?a=912288051" target="_blank" class="block">
         <div class="flex items-center justify-center h-48 bg-purple-300">
             <img src="https://wp.5balloons.info/wp-content/uploads/2022/12/3298c3eb001bbed90f1d616da66708480096a0a1b6e81bd4f8a2d6e9b831d301.webp" alt="Fathom Analytics Logo" class="block object-cover w-full h-full" />
         </div>
         <div class="text-gray-700 mb-5 text-sm px-2 text-left pt-4 pb-6"><span class="font-semibold text-lg">Make VS Code Awesome</span> - E-Book and Tutorials Make VS Code look amazing, and be fast, and efficient using it.</div>
         <div class="h-8 flex items-center justify-center w-full text-[9px] uppercase absolute bottom-0 bg-gray-100 text-indigo-600">AFFILIATED</div>
-    </a>
+    </a> -->
   </div>
   <div class="flex justify-center my-5">
       <a href="https://github.com/sponsors/tushargugnani" target="_blank">
