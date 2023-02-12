63010097d3b04a4ae65c304f6d3e6a455bc1faaf
Tushar Gugnani <tushar.gugnani@gmail.com>
2023-02-12 15:53:13 +0530

    rightsidebar

--- resources/views/partials/right-banner-bar.blade.php
+++ resources/views/partials/right-banner-bar.blade.php
@@ -1,3 +1,3 @@
-<div class="fixed hidden lg:block top-48 left-auto right-2 max-w-max z-10">
+<div class="fixed hidden lg:block top-48 left-auto right-2 max-w-max z-10" id="rightsidebar">
 <!-- Insert banner code here -->
 </div>
