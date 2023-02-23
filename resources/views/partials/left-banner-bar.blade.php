d703769460c9cbe8a0fb5098e043e03cc747e6a9
Tushar Gugnani <tushar.gugnani@gmail.com>
2023-02-23 16:22:18 +0530

    left banner bar

--- resources/views/partials/left-banner-bar.blade.php
+++ resources/views/partials/left-banner-bar.blade.php
@@ -1,12 +1,2 @@
 <div class="fixed hidden lg:block top-24 inset-0 left-2 max-w-max z-10" id="leftsidebar">
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8077071189647178"
-     crossorigin="anonymous"></script>
-<!-- Vertical Fixed Ad Unit 120 * 600 -->
-<ins class="adsbygoogle"
-     style="display:inline-block;width:120px;height:600px"
-     data-ad-client="ca-pub-8077071189647178"
-     data-ad-slot="2954531165"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
 </div>
