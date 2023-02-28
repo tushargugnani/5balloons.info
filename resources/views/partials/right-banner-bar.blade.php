541f6c398d1ca295eccfc0a4f04a4f12e44b6171
Tushar Gugnani <tushar.gugnani@gmail.com>
2023-02-28 10:09:24 +0530

    fix padding

--- resources/views/partials/right-banner-bar.blade.php
+++ resources/views/partials/right-banner-bar.blade.php
@@ -1,3 +1,3 @@
-<div class="fixed hidden lg:block top-48 left-auto right-2 max-w-max z-10" id="rightsidebar">
+<div class="fixed hidden lg:block top-24 left-auto right-2 max-w-max z-10" id="rightsidebar">
 <!-- Insert banner code here -->
 </div>
