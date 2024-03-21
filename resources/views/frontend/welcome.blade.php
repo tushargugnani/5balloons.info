c8384278ab9567e72e8d088fb6f9f2fd2d9ddb87
Tushar Gugnani <tushar.gugnani@gmail.com>
2024-03-21 21:46:45 +0530

    fix

--- resources/views/frontend/welcome.blade.php
+++ resources/views/frontend/welcome.blade.php
@@ -29,7 +29,7 @@
         </a>
     </div>
 
-    <div class="flex justify-center underline text-orange-600">
+    <div class="flex justify-center underline text-orange-600 my-5">
         <a href="https://thehome.dev" rel=”dofollow”>Curated List of Resources for Developers and Designers</a>
     </div>
 
